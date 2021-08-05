b0d3306feadf972f1c1791a39003cd9ae7c1d150
mikel-dev <ibarrondomikel@gmail.com>
2021-08-05 23:29:12 +0200

    unicafe 1.7

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -12,12 +12,42 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1)
+  const handleGoodClick = () => {
+    setGood(good + 1)
+    setAll(all + 1)
 
-  const handleNeutralClick = () => setNeutral(neutral + 1)
+    const positive = ((good + 1) * 100) / (all + 1)
+    const negative = (bad * 100) / (all + 1)
 
-  const handleBadClick = () => setBad(bad + 1)
+    setPositive(positive)
+    setAverage((positive - negative) / 100)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+
+    const positive = (good * 100) / (all + 1)
+    const negative = (bad * 100) / (all + 1)
+
+    setPositive(positive)
+    setAverage((positive - negative) / 100)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+
+    const positive = (good * 100) / (all + 1)
+    const negative = ((bad + 1) * 100) / (all + 1)
+
+    setPositive(positive)
+    setAverage((positive - negative) / 100)
+  }
 
   return (
     <div>
@@ -25,10 +55,13 @@ const App = () => {
       <Button onClick={handleGoodClick} text='Good' />
       <Button onClick={handleNeutralClick} text='Neutral' />
       <Button onClick={handleBadClick} text='Bad' />
-      <DisplayTitle title = 'statistics' />
-      <DisplayStat text = 'Good' number = {good} />
-      <DisplayStat text = 'Neutral' number = {neutral} />
-      <DisplayStat text = 'Bad' number = {bad} />
+      <DisplayTitle title='statistics' />
+      <DisplayStat text='Good' number={good} />
+      <DisplayStat text='Neutral' number={neutral} />
+      <DisplayStat text='Bad' number={bad} />
+      <DisplayStat text='All' number={all} />
+      <DisplayStat text='Average' number={average} />
+      <DisplayStat text='Positive' number={positive + ' %'} />
     </div>
   )
 }
