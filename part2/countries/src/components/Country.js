3a080146195b56a9459f14782f4bb729e27e6c76
mikel-dev <ibarrondomikel@gmail.com>
2021-08-29 19:18:02 +0200

    countries 2.14

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
 
 const Country = ({country}) => {
 
+    const [ weather, setWeather ] = useState('')
+
+    useEffect(() => {
+        const params = {
+            access_key: process.env.REACT_APP_API_KEY,
+            query: country.name
+        }
+
+        axios
+          .get('http://api.weatherstack.com/current', {params})
+          .then(response => {
+            setWeather(response.data.current)
+          })
+    }, [country.name])
+
     return (
         <div>
-            <div>
             <h1>{country.name}</h1>
             <div>Capital: {country.capital}</div>
             <div>Population: {country.population}</div>
@@ -15,7 +30,17 @@ const Country = ({country}) => {
                 )}
             </ul>
             <img src={country.flag} alt='Flag' width='100px' />
-            </div>
+            {weather ?
+              <div>
+                <h3>Weather in {country.name}</h3>
+                <div>Temperature: {weather.temperature} Celcius</div>
+                {weather.weather_icons.map(icon =>
+                    <img key={icon} src={icon} alt='weather' width='100px' />
+                )}
+                <div>Wind: {weather.wind_speed} mph direction {weather.wind_dir}</div>
+              </div> : 
+              <div></div>
+            }
         </div>      
     )
 }
